bedd522fb44c46af95a701d57e72a6d070691124
Abhinaba Ghosh <askabhinaba@gmail.com>
2020-07-03 14:54:35 +0530

    Update input-field-interactors.ts

--- core/utils/elemHelper/models/input-field-interactors.ts
+++ core/utils/elemHelper/models/input-field-interactors.ts
@@ -1,5 +1,7 @@
 /* tslint:disable */
-import { browser, ElementFinder } from 'protractor';
+import chalk from 'chalk';
+import { browser, ElementFinder, protractor } from 'protractor';
+import * as webdriver from 'selenium-webdriver';
 
 import { timeout } from '../utils/validator';
 import { browserWaitElementVisible } from './waiters';
@@ -15,36 +17,35 @@ import { browserWaitElementVisible } from './waiters';
  * const buttonElement=element(by.id('input[class='abc']'));
  * await p.typeValue(buttonElement,"hello");
  */
-export const typeValue = async (
-  webElement: ElementFinder,
+
+export const typeValue = (
+  htmlElement: ElementFinder,
   value: string,
-  sleepTime = 1000,
-  tryCount: number = (Number(process.env.IMPLICIT_WAIT) ||
-    timeout.timeoutInMilliseconds) / sleepTime || 10
-): Promise<void | boolean> =>
-  webElement.sendKeys(value).then(
-    () => true,
-    (error) => {
-      // tslint:disable-next-line: no-console
-      console.log(
-        `re-trying sending text to Element ${webElement.parentElementArrayFinder.locator_}, try ${tryCount} times more`
-      );
+  timeout: number = Math.floor(Number(process.env.IMPLICIT_WAIT) / Number(process.env.TEST_RETRY_COUNT)) || 5000,
+  tryCount: number = Number(process.env.TEST_RETRY_COUNT) || 10
+): webdriver.promise.Promise<void> => {
+  return browser.wait(protractor.ExpectedConditions.presenceOf(htmlElement), timeout, `Element ${htmlElement.locator()} is not present`).then(
+    () => {
+      htmlElement.sendKeys(value);
+    },
+    (error: any) => {
       if (tryCount > 0) {
-        return browser.sleep(sleepTime).then(() => {
-          return typeValue(webElement, value, 1000, tryCount - 1);
-        });
+        console.log(chalk.yellow(`Send keys retry ${tryCount} on target ${htmlElement.locator()}`));
+        typeValue(htmlElement, value, timeout, tryCount - 1);
       } else {
+        console.error(chalk.redBright(`Error while sending keys on ${htmlElement.locator()}`));
         throw error;
       }
     }
   );
+};
 
 /**
  * this method helps to type a date value in date input
  * @param dateInput
  * @param date
  */
-export const typeDate = async (dateInput: ElementFinder, date: string) => {
+export const typeDate = async (dateInput: ElementFinder, date: string): Promise<void> => {
   await dateInput.click();
   await dateInput.clear();
   return dateInput.sendKeys(date);
@@ -57,136 +58,99 @@ export const typeDate = async (dateInput: ElementFinder, date: string) => {
  */
 export const clear = async (
   webElement: ElementFinder,
-  timeoutInMilliseconds: number = Number(process.env.IMPLICIT_WAIT) ||
-    timeout.timeoutInMilliseconds
-) => {
+  timeoutInMilliseconds: number = Number(process.env.IMPLICIT_WAIT) || timeout.timeoutInMilliseconds
+): Promise<void> => {
   await browserWaitElementVisible(webElement, timeoutInMilliseconds);
   await webElement.clear();
 };
 
 /**
  * this element helps to get the attribute value of an element
- * @param element
- * @param attrName
+ * @param htmlElement
+ * @param attributeName
+ * @param timeout
+ * @param tryCount
  */
-export const getAttribute = async (
+export const getAttribute = (
   htmlElement: ElementFinder,
   attributeName: string,
-  sleepTime = 1000,
-  tryCount: number = (Number(process.env.IMPLICIT_WAIT) ||
-    timeout.timeoutInMilliseconds) / sleepTime || 10
-): Promise<string> =>
-  htmlElement.getAttribute(attributeName).then(
-    (text) => {
-      if (text && text !== '') {
-        return text;
-      } else {
-        console.log(
-          `received attribute value as null, re-trying ${tryCount} times more`
-        );
-        if (tryCount > 0) {
-          return browser.sleep(sleepTime).then(() => {
-            return getAttribute(htmlElement, attributeName, 1000, tryCount - 1);
-          });
-        } else {
-          return text;
+  timeout: number = Math.floor(Number(process.env.IMPLICIT_WAIT) / Number(process.env.TEST_RETRY_COUNT)) || 5000,
+  tryCount: number = Number(process.env.TEST_RETRY_COUNT) || 10
+): webdriver.promise.Promise<string> => {
+  return browser
+    .wait(protractor.ExpectedConditions.presenceOf(htmlElement), timeout, `Element ${htmlElement.locator()} is not present`)
+    .then(() => {
+      return htmlElement.getAttribute(attributeName);
+    })
+    .then(
+      (value: string) => value,
+      (error: any) => {
+        if (tryCount === 0) {
+          console.error(chalk.redBright(`Error while getting attribute value on ${htmlElement.locator()}`));
+          throw error;
         }
+        console.log(chalk.yellow(`getAttribute retry ${tryCount} on target ${htmlElement.locator()}`));
+        return getAttribute(htmlElement, attributeName, timeout, tryCount - 1);
       }
-    },
-    (error) => {
-      console.log(
-        `re-trying fetching attribute from Element ${htmlElement.parentElementArrayFinder.locator_}, try ${tryCount} times more`
-      );
-      if (tryCount > 0) {
-        return browser.sleep(sleepTime).then(() => {
-          return getAttribute(htmlElement, attributeName, 1000, tryCount - 1);
-        });
-      } else {
-        throw error;
-      }
-    }
-  );
-/**
- * this method helps to get the css value of an element
- * @param element
- * @param cssValue
- */
-export const getCssValue = async (
-  htmlElement: ElementFinder,
-  cssValue: string,
-  sleepTime = 1000,
-  tryCount: number = (Number(process.env.IMPLICIT_WAIT) ||
-    timeout.timeoutInMilliseconds) / sleepTime || 10
-): Promise<string> =>
-  htmlElement.getCssValue(cssValue).then(
-    (text) => {
-      if (text && text !== '') {
-        return text;
-      } else {
-        console.log(
-          `received css value as null, re-trying ${tryCount} times more`
-        );
-        if (tryCount > 0) {
-          return browser.sleep(sleepTime).then(() => {
-            return getAttribute(htmlElement, cssValue, 1000, tryCount - 1);
-          });
-        } else {
-          return text;
-        }
-      }
-    },
-    (error) => {
-      console.log(
-        `re-trying fetching css value from Element ${htmlElement.parentElementArrayFinder.locator_}, try ${tryCount} times more`
-      );
-      if (tryCount > 0) {
-        return browser.sleep(sleepTime).then(() => {
-          return getCssValue(htmlElement, cssValue, 1000, tryCount - 1);
-        });
-      } else {
-        throw error;
-      }
-    }
-  );
+    );
+};
 
 /**
  * this method helps to get the css value of an element
- * @param element
+ * @param htmlElement
  * @param cssValue
+ * @param timeout
+ * @param tryCount
  */
-export const getText = async (
+export const getCssValue = (
   htmlElement: ElementFinder,
-  sleepTime = 1000,
-  tryCount: number = (Number(process.env.IMPLICIT_WAIT) ||
-    timeout.timeoutInMilliseconds) / sleepTime || 10
-): Promise<string> =>
-  htmlElement.getText().then(
-    (text) => {
-      if (text && text !== '') {
-        return text;
-      } else {
-        console.log(
-          `received getText() as null, re-trying ${tryCount} times more`
-        );
-        if (tryCount > 0) {
-          return browser.sleep(sleepTime).then(() => {
-            return getText(htmlElement, 1000, tryCount - 1);
-          });
-        } else {
-          return text;
+  cssValue: string,
+  timeout: number = Math.floor(Number(process.env.IMPLICIT_WAIT) / Number(process.env.TEST_RETRY_COUNT)) || 5000,
+  tryCount: number = Number(process.env.TEST_RETRY_COUNT) || 10
+): webdriver.promise.Promise<string> => {
+  return browser
+    .wait(protractor.ExpectedConditions.presenceOf(htmlElement), timeout, `Element ${htmlElement.locator()} is not present`)
+    .then(() => {
+      return htmlElement.getCssValue(cssValue);
+    })
+    .then(
+      (value: string) => value,
+      (error: any) => {
+        if (tryCount === 0) {
+          console.error(chalk.redBright(`Error while getting css value on target ${htmlElement.locator()}`));
+          throw error;
         }
+        console.log(chalk.yellow(`getCssValue retry ${tryCount} on target ${htmlElement.locator()}v`));
+        return getCssValue(htmlElement, cssValue, timeout, tryCount - 1);
       }
-    },
-    (error) => {
-      console.log(
-        `re-trying fetching text from Element ${htmlElement.parentElementArrayFinder.locator_}, try ${tryCount} times more`
-      );
-      if (tryCount > 0) {
-        return browser.sleep(sleepTime).then(() => {
-          return getText(htmlElement, 1000, tryCount - 1);
-        });
-      } else {
-        throw error;
+    );
+};
+
+/**
+ * this method helps to get the text value of an element
+ * @param htmlElement
+ * @param timeout
+ * @param tryCount
+ */
+export const getText = (
+  htmlElement: ElementFinder,
+  timeout: number = Math.floor(Number(process.env.IMPLICIT_WAIT) / Number(process.env.TEST_RETRY_COUNT)) || 5000,
+  tryCount: number = Number(process.env.TEST_RETRY_COUNT) || 10
+): webdriver.promise.Promise<string> => {
+  return browser
+    .wait(protractor.ExpectedConditions.presenceOf(htmlElement), timeout, `Element ${htmlElement.locator()} is not present`)
+    .then(() => {
+      return htmlElement.getText();
+    })
+    .then(
+      (value: string) => value,
+      (error: any) => {
+        if (tryCount === 0) {
+          console.error(chalk.redBright(`Error while getting text on ${htmlElement.locator()}`));
+          throw error;
+        }
+        console.log(chalk.yellow(`getText retry ${tryCount} on ${htmlElement.locator()}`));
+        return getText(htmlElement, timeout, tryCount - 1);
       }
-    }
-  );
+    );
+};
